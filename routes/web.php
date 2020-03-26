8b378c0e29745fe45554712e6c770d05c5fad2e9
thinnd <thin-nd@gmail.com>
2020-03-26 11:01:12 +0700

    push form admin

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,30 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function () {
+
+    // crud teacher by admin
+    Route::get('get-teacher', function (){
+        echo "23";
+    });
+    Route::post('update-teacher');
+    Route::post('new-teacher');
+    Route::post('delete-teacher');
+
+    // crud student by admin
+    Route::get('get-student', function (){
+        echo "23";
+    });
+    Route::post('update-student');
+    Route::post('new-student');
+    Route::post('delete-student');
+
+    // crud subject by admin
+    Route::get('get-subject', function (){
+        echo "23";
+    });
+    Route::post('update-subject');
+    Route::post('new-subject');
+    Route::post('delete-subject');
+});
